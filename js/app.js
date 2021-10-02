9853ae2398422546692dcb18abe8431ad5c1523e
Santo <santokhan1999@gmail.com>
2021-10-02 21:22:05 +0600

    Add: index.html, manifest.json, bootstrap, fontawesome, style.css, app.js, jquery.js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/**Dom Selection */
+const navBtn = document.getElementById("elementId");
+
+
+/**Function Call Automatically*/
+(function () {
+
+})();
+
+/**Normal Function */
+function myFunc() {
+
+}
+
+/**Arrow Function */
+var arrowFunc = (e) => {
+    /**Your code here */
+    return e;
+}
